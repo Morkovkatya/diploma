84eaa8692c2b61d2a4ad372923c3e9c8dcafee1c
Morkovkatya <Saltanovak2002@gmail.com>
2024-06-10 08:08:16 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const admins = {
-    "admin@example.com": "admin123"
+const teachers = {
+    "teacher1@mail.ru": "teacher123",
+    "teacher2@mail.ru": "teacher456"
 };
 
 const users = {
-    "user1@example.com": { password: "user123", group: "ПКТб-20-1", name: "Волков Владислав Владимирович" },
-    "user2@example.com": { password: "user456", group: "ПКТб-20-1", name: "Быкова Ирина Геннадьевна" },
-    "user3@example.com": { password: "user789", group: "МКМб-20-1", name: "Мамедов Вадим Николаевич" }
+    "user1@mail.ru": { password: "user123", group: "ПКТб-20-1", name: "Волков Владислав Владимирович" },
+    "user2@mail.ru": { password: "user456", group: "ПКТб-20-1", name: "Быкова Ирина Геннадьевна" },
+    "user3@mail.ru": { password: "user789", group: "МКМб-20-1", name: "Мамедов Вадим Николаевич" }
 };
-
 let token = localStorage.getItem('token');
 let role = localStorage.getItem('role');
 let currentUser = localStorage.getItem('currentUser');
 
 if (token && role) {
-    if (role === 'admin') {
+    if (role === 'teacher') {
         showGroupSelect();
     } else {
         showGradebook();
@@ -26,11 +26,12 @@ function login() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    if (admins[email] && admins[email] === password) {
-        role = "admin";
+    if (teachers[email] && teachers[email] === password) {
+        role = "teacher";
         token = generateJWT(email);
         localStorage.setItem('token', token);
         localStorage.setItem('role', role);
+        localStorage.setItem('currentUser', email);
         document.getElementById('login-container').classList.add('hidden');
         showGroupSelect();
     } else if (users[email] && users[email].password === password) {
@@ -47,7 +48,6 @@ function login() {
 }
 
 function generateJWT(email) {
-    // Для простоты используем base64 кодирование в качестве фиктивного JWT
     return btoa(JSON.stringify({ email }));
 }
 
@@ -57,6 +57,9 @@ function showGroupSelect() {
 
     const groups = new Set(Object.values(users).map(user => user.group));
     const groupSelect = document.getElementById('group-select');
+    
+    // Очищаем все предыдущие опции
+    groupSelect.innerHTML = '';
 
     groups.forEach(group => {
         const option = document.createElement('option');
@@ -65,7 +68,7 @@ function showGroupSelect() {
         groupSelect.appendChild(option);
     });
 
-    loadStudents(); // Load students for the first group by default
+    loadStudents();
 }
 
 function loadStudents() {
@@ -87,10 +90,10 @@ function showGradebook() {
     document.getElementById('group-select-container').classList.add('hidden');
     document.getElementById('gradebook-container').classList.remove('hidden');
 
-    if (role === 'admin') {
+    if (role === 'teacher') {
         const selectedStudent = document.getElementById('student-select').value;
         currentUser = selectedStudent;
-        document.getElementById('admin-controls').classList.remove('hidden');
+        document.getElementById('teacher-controls').classList.remove('hidden');
     } else {
         currentUser = localStorage.getItem('currentUser');
     }
@@ -105,22 +108,9 @@ function formatDate(dateString) {
 
 function loadGradebook() {
     const tbody = document.getElementById('gradebook').getElementsByTagName('tbody')[0];
-    tbody.innerHTML = ''; // Очистить существующие строки
+    tbody.innerHTML = '';
 
     let subjects = JSON.parse(localStorage.getItem(`subjects_${currentUser}`));
-
-    if (!subjects || !Array.isArray(subjects) || !subjects.length || !subjects[0].hasOwnProperty('hours')) {
-        // Если данные отсутствуют или имеют старый формат, инициализировать их новыми данными
-        subjects = [
-            { subject: 'Математический анализ', hours: 120, score: 91, date: '2024-06-12', teacher: 'Иванов И.И.' },
-            { subject: 'Английский язык', hours: 80, score: 88, date: '2024-06-12', teacher: 'Петрова А.А.' },
-            { subject: 'Физическая культура', hours: 60, score: 95, date: '2024-06-12', teacher: 'Сидоров С.С.' },
-            { subject: 'Философия', hours: 100, score: 76, date: '2024-06-12', teacher: 'Кузнецова Н.Н.' },
-            { subject: 'Инженерная графика', hours: 90, score: 82, date: '2024-06-12', teacher: 'Морозов В.В.' },
-            { subject: 'Машинное обучение', hours: 110, score: 89, date: '2024-06-12', teacher: 'Смирнов Д.Н.' }
-        ];
-        saveData(subjects);
-    }
 
     subjects.forEach(subj => {
         const row = tbody.insertRow();
@@ -134,14 +124,19 @@ function loadGradebook() {
         cell2.innerHTML = subj.hours;
         cell3.innerHTML = subj.score;
         cell4.innerHTML = formatDate(subj.date);
-        cell5.innerHTML = subj.teacher;
+        cell5.innerHTML = subj.teacherName;
 
-        if (role === 'admin') {
+        if (role === 'teacher' && subj.teacher === localStorage.getItem('currentUser')) {
+            cell1.contentEditable = true;
             cell2.contentEditable = true;
             cell3.contentEditable = true;
             cell4.contentEditable = true;
             cell5.contentEditable = true;
 
+            cell1.addEventListener('blur', () => {
+                subj.subject = cell1.innerHTML;
+                saveData(subjects);
+            });
             cell2.addEventListener('blur', () => {
                 subj.hours = cell2.innerHTML;
                 saveData(subjects);
@@ -155,7 +150,7 @@ function loadGradebook() {
                 saveData(subjects);
             });
             cell5.addEventListener('blur', () => {
-                subj.teacher = cell5.innerHTML;
+                subj.teacherName = cell5.innerHTML;
                 saveData(subjects);
             });
         }
@@ -176,7 +171,7 @@ function addSubject() {
 
     if (newSubject && newHours && newScore && newDate && newTeacher) {
         const subjects = JSON.parse(localStorage.getItem(`subjects_${currentUser}`)) || [];
-        subjects.push({ subject: newSubject, hours: newHours, score: newScore, date: newDate, teacher: newTeacher });
+        subjects.push({ subject: newSubject, hours: newHours, score: newScore, date: newDate, teacherName: newTeacher, teacher: localStorage.getItem('currentUser') });
         saveData(subjects);
         loadGradebook();
     }
@@ -184,6 +179,12 @@ function addSubject() {
 
 function saveData(data) {
     localStorage.setItem(`subjects_${currentUser}`, JSON.stringify(data));
+}
+
+function back() {
+    document.getElementById('gradebook-container').classList.add('hidden');
+    document.getElementById('teacher-controls').classList.add('hidden');
+    document.getElementById('group-select-container').classList.remove('hidden');
 }
 
 function logout() {
@@ -195,7 +196,6 @@ function logout() {
     localStorage.removeItem('currentUser');
     document.getElementById('login-container').classList.remove('hidden');
     document.getElementById('gradebook-container').classList.add('hidden');
-    document.getElementById('admin-controls').classList.add('hidden');
+    document.getElementById('teacher-controls').classList.add('hidden');
     document.getElementById('group-select-container').classList.add('hidden');
 }
-
